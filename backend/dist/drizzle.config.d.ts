38ca71cd435fca48463942e6c8e175ed2d73a3d3
Shinyaev Vladimir <secman@bk.ru>
2025-04-24 10:56:44 +0500

    add tailwind and postcss configuration

---
+++ backend/dist/drizzle.config.d.ts
@@ -0,0 +1,15 @@
+declare const _default: {
+    dialect: "postgresql";
+    schema: string;
+    dbCredentials: {
+        host: string;
+        port: number;
+        database: string;
+        user: string;
+        password: string;
+    };
+    out: string;
+    verbose: true;
+    strict: true;
+};
+export default _default;
---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	let { children } = $props();
+	import '$lib/accets/css/app.css';
+</script>
+
+{@render children()}
